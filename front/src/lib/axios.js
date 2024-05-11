953f1e30572fdd828ed3882a9e9af2f568896b12
daw2072024 <nestor.defrutos.alonso@iesjulianmarias.es>
2024-05-11 18:49:29 +0200

    change ip in .env

--- front/src/lib/axios.js
+++ front/src/lib/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: "http://79.143.93.91/back", //docker
+    baseURL: "http://79.143.91.167/back", //docker test
+    // baseURL: "http://79.143.93.91/back", //docker prod
     //  baseURL: "http://localhost:8000",//local
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
